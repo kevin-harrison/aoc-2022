0c1c0f5ef9de83e4f2dc514532736fa0a5145c17
kevin <kevinha@kth.se>
2023-01-03 01:58:15 +0100

    started day 10

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,6 +1,30 @@
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<i32> {
+    let input = &input[..input.len()-1];
+    let mut x = 1;
+    let mut cycle = 1;
+    let mut answer = 0;
+
+    for line in input.split("\n") {
+        //println!("{:<9} cycle {}: x={}", line, cycle, x);
+
+        // start of cycle
+        let operation = &line[..4];
+        match operation {
+            "noop" => cycle += 1,
+            "addx" => {
+                let operand = line[5..].parse::<i32>().ok()?;
+                cycle += 1;
+                if (cycle - 20) % 40 == 0 {answer += x * cycle;println!("{:<9} cycle {}: x={}", line, cycle, x);}
+                cycle += 1;
+                x += operand;
+            },
+            _ => panic!("Unsupported operation")
+        }
+        if (cycle - 20) % 40 == 0 {answer += x * cycle;println!("{:<9} cycle {}: x={}", line, cycle, x);}
+    }
+    Some(answer)
 }
+
 
 pub fn part_two(input: &str) -> Option<u32> {
     None
@@ -19,7 +43,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 10);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(13140));
     }
 
     #[test]
